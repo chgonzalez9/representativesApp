fa487cb6c58173ff7357a3743902e24cb3cd0a97
Christian <thech999@gmail.com>
2022-09-17 03:58:10 +0100

    RepresentativeListAdapter.kt fixed

--- app/src/main/java/com/example/android/politicalpreparedness/representative/adapter/RepresentativeListAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/adapter/RepresentativeListAdapter.kt
@@ -11,11 +11,12 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.politicalpreparedness.R
-import com.example.android.politicalpreparedness.databinding.ViewholderRepresentativeBinding
+import com.example.android.politicalpreparedness.databinding.RepresentativeListItemBinding
 import com.example.android.politicalpreparedness.network.models.Channel
 import com.example.android.politicalpreparedness.representative.model.Representative
 
-class RepresentativeListAdapter: ListAdapter<Representative, RepresentativeViewHolder>(RepresentativeDiffCallback()){
+class RepresentativeListAdapter(val onClickListener: OnClickListener) :
+    ListAdapter<Representative, RepresentativeListAdapter.RepresentativeViewHolder>(RepresentativeDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepresentativeViewHolder {
         return RepresentativeViewHolder.from(parent)
@@ -23,61 +24,95 @@ class RepresentativeListAdapter: ListAdapter<Representative, RepresentativeViewH
 
     override fun onBindViewHolder(holder: RepresentativeViewHolder, position: Int) {
         val item = getItem(position)
+        holder.itemView.setOnClickListener {
+            onClickListener.onClick(item)
+        }
         holder.bind(item)
     }
-}
 
-class RepresentativeViewHolder(val binding: ViewholderRepresentativeBinding): RecyclerView.ViewHolder(binding.root) {
+    class RepresentativeViewHolder(val binding: RepresentativeListItemBinding): RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(item: Representative) {
-        binding.representative = item
-        binding.representativePhoto.setImageResource(R.drawable.ic_profile)
+        fun bind(item: Representative) {
+            binding.representative = item
+            binding.representativeIcon.setImageResource(R.drawable.ic_profile)
+            binding.webIcon.setOnClickListener {
+                binding.representative.official.urls?.let { it -> showWWWLinks(it) }
+            }
+            binding.facebookIcon.setOnClickListener {
+                showSocialLinks()
+            }
 
-        //TODO: Show social links ** Hint: Use provided helper methods
-        //TODO: Show www link ** Hint: Use provided helper methods
+            //TODO: Show social links ** Hint: Use provided helper methods
+            //TODO: Show www link ** Hint: Use provided helper methods
 
-        binding.executePendingBindings()
-    }
+            binding.executePendingBindings()
+        }
 
-    //TODO: Add companion object to inflate ViewHolder (from)
+        companion object {
+            fun from(parent: ViewGroup): RepresentativeViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = RepresentativeListItemBinding.inflate(layoutInflater, parent, false)
 
-    private fun showSocialLinks(channels: List<Channel>) {
-        val facebookUrl = getFacebookUrl(channels)
-        if (!facebookUrl.isNullOrBlank()) { enableLink(binding.facebookIcon, facebookUrl) }
+                return RepresentativeViewHolder(view)
+            }
+        }
 
-        val twitterUrl = getTwitterUrl(channels)
-        if (!twitterUrl.isNullOrBlank()) { enableLink(binding.twitterIcon, twitterUrl) }
-    }
+        private fun showSocialLinks(channels: List<Channel>) {
+            val facebookUrl = getFacebookUrl(channels)
+            if (!facebookUrl.isNullOrBlank()) { enableLink(binding.facebookIcon, facebookUrl) }
 
-    private fun showWWWLinks(urls: List<String>) {
-        enableLink(binding.wwwIcon, urls.first())
-    }
+            val twitterUrl = getTwitterUrl(channels)
+            if (!twitterUrl.isNullOrBlank()) { enableLink(binding.twitterIcon, twitterUrl) }
+        }
 
-    private fun getFacebookUrl(channels: List<Channel>): String? {
-        return channels.filter { channel -> channel.type == "Facebook" }
+        private fun showWWWLinks(urls: List<String>) {
+            enableLink(binding.webIcon, urls.first())
+        }
+
+        private fun getFacebookUrl(channels: List<Channel>): String? {
+            return channels.filter { channel -> channel.type == "Facebook" }
                 .map { channel -> "https://www.facebook.com/${channel.id}" }
                 .firstOrNull()
-    }
+        }
 
-    private fun getTwitterUrl(channels: List<Channel>): String? {
-        return channels.filter { channel -> channel.type == "Twitter" }
+        private fun getTwitterUrl(channels: List<Channel>): String? {
+            return channels.filter { channel -> channel.type == "Twitter" }
                 .map { channel -> "https://www.twitter.com/${channel.id}" }
                 .firstOrNull()
+        }
+
+        private fun enableLink(view: ImageView, url: String) {
+            view.visibility = View.VISIBLE
+            view.setOnClickListener { setIntent(url) }
+        }
+
+        private fun setIntent(url: String) {
+            val uri = Uri.parse(url)
+            val intent = Intent(ACTION_VIEW, uri)
+            itemView.context.startActivity(intent)
+        }
+
     }
 
-    private fun enableLink(view: ImageView, url: String) {
-        view.visibility = View.VISIBLE
-        view.setOnClickListener { setIntent(url) }
+    class RepresentativeDiffCallback : DiffUtil.ItemCallback<Representative>() {
+        override fun areItemsTheSame(
+            oldItem: Representative,
+            newItem: Representative
+        ): Boolean {
+            return oldItem.official.name == newItem.official.name
+        }
+
+        override fun areContentsTheSame(
+            oldItem: Representative,
+            newItem: Representative
+        ): Boolean {
+            return oldItem == newItem
+        }
+
     }
 
-    private fun setIntent(url: String) {
-        val uri = Uri.parse(url)
-        val intent = Intent(ACTION_VIEW, uri)
-        itemView.context.startActivity(intent)
+    class OnClickListener(val clickListener: (representative: Representative) -> Unit) {
+        fun onClick(representative: Representative) = clickListener(representative)
     }
 
 }
-
-//TODO: Create RepresentativeDiffCallback
-
-//TODO: Create RepresentativeListener
