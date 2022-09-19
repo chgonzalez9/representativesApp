3c36df3f9ca7b0d5adf479114f99f6dc55ecfe00
christian <thech999@gmail.com>
2022-09-19 20:46:12 +0100

    Room DAO implemented

--- app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -1,7 +1,10 @@
 package com.example.android.politicalpreparedness.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.example.android.politicalpreparedness.network.models.Election
@@ -10,13 +13,22 @@ import com.example.android.politicalpreparedness.network.models.Election
 interface ElectionDao {
 
     //TODO: Add insert query
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg election: Election)
 
     //TODO: Add select all election query
+    @Query("SELECT * FROM election_table")
+    fun getALl() : LiveData<List<Election>>
 
     //TODO: Add select single election query
+    @Query("SELECT * FROM election_table")
+    suspend fun get(id: Int): Election?
 
     //TODO: Add delete query
+    @Delete
+    suspend fun deleteElection(election: Election)
 
     //TODO: Add clear query
-
+    @Query("DELETE FROM election_table")
+    suspend fun clearAll()
 }
